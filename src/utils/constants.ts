ae756139c61adaad357d94d1cd334f7f19cae101
Palanikannan1437 <73993394+Palanikannan1437@users.noreply.github.com>
2023-05-05 16:32:32 +0530

    fixed constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
-// export const NEXT_PUBLIC_API_URL = "https://mochi-api.vercel.app"
-export const NEXT_PUBLIC_API_URL = "http://localhost:3000"
+export const NEXT_PUBLIC_API_URL = "https://mochi-api.vercel.app"
+// export const NEXT_PUBLIC_API_URL = "http://localhost:3000"
